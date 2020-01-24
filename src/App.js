5ed65e031c5170fd828198dcd013a5d88f9c2696
Gustavo Herrera <tavoxr23@gmail.com>
2020-01-23 23:01:44 -0600

    search component and state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import Search from './components/Search';
 
-function App() {
+class App extends React.Component {
+
+  state={
+      termino:''
+  }
+
+  datosBusqueda=(termino)=>{
+    console.log(termino);
+    this.setState({
+      termino
+    })
+  }
+
+
+render(){
   return (
     <div className="app container">
       <div className='jumbotron'>
         <p className='lead text-center'>Image Search</p>
-        <Search/>
+        <Search
+            datosBusqueda={this.datosBusqueda}
+        
+        />
       </div>
+      {this.state.termino}
     </div>
   );
+}
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,40 @@ import React from 'react';
 
 
 class Search extends React.Component{
+
+    searchRef= React.createRef();
+
+   
+
+
+    obtenerDatos=(e)=>{
+        e.preventDefault();
+
+        //tomamos el valor del input
+        const termino= this.searchRef.current.value;
+
+        //lo enviamos al componente principal
+        this.props.datosBusqueda(termino)
+    }
+
+
     render(){
         return(
-            <form>
+            <form onSubmit={this.obtenerDatos} >
                 <div className='row'>
                     <div className='form-group col-md-8'>
                         <input type='text' 
+                                ref={this.searchRef}
                                 className='form-control form-control-lg' 
                                 placeholder='Search your image. Example: Coffee'
+                        />
+                        
+                    </div>
+
+                    <div className='form-group col-md-4'>
+                        <input type='submit' 
+                                className='btn btn-lg btn-info btn-block' 
+                                value='Search'
                         />
                         
                     </div>
